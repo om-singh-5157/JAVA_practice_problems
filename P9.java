0b3b9e22730146bfe19b4e13a3681fbee3356f1d
OM SINGH <136950749+om-singh-5157@users.noreply.github.com>
2025-02-21 18:20:43 +0530

    adding new file

---
+++ P9.java
@@ -0,0 +1,15 @@
+public class P9 {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++){
+            for(int j=1;j<=7;j++){
+                if ((i<=3&&i==j)||(i<=3&&(i+j)==8)||(j==4&&i>=4)) {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
