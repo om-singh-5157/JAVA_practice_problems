9d9ae4ef249d79cd12803748dfc826cbb3b6b6d1
OM SINGH <136950749+om-singh-5157@users.noreply.github.com>
2025-03-01 23:16:00 +0530

    adding new file

---
+++ 8 Arithmetic.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+class Arithmetic {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter first no.:");
+        int x= input.nextInt();
+        System.out.print("Enter second no.:");
+        int y= input.nextInt();
+        System.out.println("sum = "+(x+y));
+        System.out.println("sub = "+(x-y));
+        System.out.println("mul = "+(x*y));
+        System.out.println("div = "+(x/y));
+        System.out.println("mod = "+(x%y));
+    }
+}
