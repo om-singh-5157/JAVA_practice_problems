1a2fb5ee579308cd14b40da711225c4417c771ee
OM SINGH <136950749+om-singh-5157@users.noreply.github.com>
2025-03-01 23:17:19 +0530

    adding new file

---
+++ ArrayUtility.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayUtility {
+    public static int[] inputArray(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array:");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        int i=0;
+        while(i<size){
+            System.out.print("Enter the element no "+i+" :");
+            arr[i] = input.nextInt();
+            i++;
+        }
+        return arr;
+    }
+}
