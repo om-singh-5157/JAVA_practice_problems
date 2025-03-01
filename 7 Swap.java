1288cd68f38d8453d203991294ed4107b93abf2d
OM SINGH <136950749+om-singh-5157@users.noreply.github.com>
2025-03-01 23:15:25 +0530

    adding new file

---
+++ 7 Swap.java
@@ -0,0 +1,11 @@
+class Swap {
+    public static void main(String[] args) {
+        int x=9;
+        int y=19;
+        int z;
+        z=x;
+        x=y;
+        y=z;  
+        System.out.println("x="+x+", y="+y); 
+    }
+}
