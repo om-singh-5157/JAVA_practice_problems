ce3a1a18b2e6497366cffed743731465db7b2741
OM SINGH <136950749+om-singh-5157@users.noreply.github.com>
2025-03-01 23:16:39 +0530

    adding new file

---
+++ 10 PerimeterRec.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class PerimeterRec {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the length:");
+        double l=input.nextInt();
+        System.out.print("Enter the breadth:");
+        double b=input.nextInt();
+        System.out.println("Perimeter of ur rectangle is = "+(2*(l+b)));
+    }
+}
