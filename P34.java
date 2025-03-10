c07fff44fae451e253033f695bd66d991e0cdb7d
OM SINGH <136950749+om-singh-5157@users.noreply.github.com>
2025-03-10 22:48:23 +0530

    Update P34.java

--- P34.java
+++ P34.java
@@ -37,14 +37,14 @@ public class P34 {
             }
             System.out.println();
         }
-        // for(int i=5;i>=1;i--){
-        //     for(int j=5;j>i;j--){
-        //         System.out.print(" ");
-        //     }
-        //     for(int k=i;k>=1;k--){
-        //         System.out.print(k);
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=5;i>=1;i--){
+            for(int j=5;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=i;k>=1;k--){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
     }
 }
